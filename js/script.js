0dfa669e83e45bc97eb19bb201a6e80a3cc7e48f
Mirco Borella <43444087+Mircodj@users.noreply.github.com>
2025-03-10 19:39:42 +0100

    Testing nuove feature (#2)

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var acc = document.getElementsByClassName("accordionButton");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var accordionContent = this.nextElementSibling;
+    if (accordionContent.style.display === "block") {
+      accordionContent.style.display = "none";
+    } else {
+      accordionContent.style.display = "block";
+    }
+  });
+}
+
+
+var words = ["Innovazione", "Tecnologia", "Sviluppo", "Crescita", "Successo"];
+var i = 0;
+var txtIndex = 0;
+
+function typingEffect() {
+  if (i < words[txtIndex].length) {
+    document.getElementById("typingEffect").innerHTML += words[txtIndex].charAt(i);
+    i++;
+    setTimeout(typingEffect, 150);
+  } else {
+    i = 0;
+    txtIndex++;
+    if (txtIndex < words.length) {
+      document.getElementById("typingEffect").innerHTML = "";
+      setTimeout(typingEffect, 150);
+    }
+  }
+  if (txtIndex == words.length) {
+    txtIndex = 0;
+    document.getElementById("typingEffect").innerHTML = "";
+    setTimeout(typingEffect, 150);
+  }
+}
+
+typingEffect();
