d85aac6dfef2f4391834ced7b70bb820dee5bebb
Mirco Borella <43444087+Mircodj@users.noreply.github.com>
2025-04-09 10:29:15 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 //-----------------------------------
 //-           Accordions            -
 //-----------------------------------
-var acc = document.getElementsByClassName("accordionButton");
-
-for (var i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var accordionContent = this.nextElementSibling;
-    if (accordionContent.style.display === "block") {
-      accordionContent.style.display = "none";
-    } else {
-      accordionContent.style.display = "block";
-    }
-  });
-}
+//var acc = document.getElementsByClassName("accordionButton");
+//
+//for (var i = 0; i < acc.length; i++) {
+//  acc[i].addEventListener("click", function() {
+//    var accordionContent = this.nextElementSibling;
+//    if (accordionContent.style.display === "block") {
+//      accordionContent.style.display = "none";
+//    } else {
+//      accordionContent.style.display = "block";
+//    }
+//  });
+//}
 
 
 
